497c4a07048e3284dbe29f7837cfa7bbfc96077d
Fumiya Tokumasu <toof@toof.jp>
2025-12-10 18:00:21 +0900

    refactor: format

--- terraform/project.tf
+++ terraform/project.tf
@@ -1,5 +1,5 @@
 resource "google_project" "toof_infra" {
-  name       = "toof-infra"
-  project_id = "toof-infra"
+  name            = "toof-infra"
+  project_id      = "toof-infra"
   billing_account = var.google_billing_account
 }
