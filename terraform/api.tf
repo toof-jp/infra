7b5766ddf29b4d07520f43af420eae8a510fb7b7
Fumiya Tokumasu <toof@toof.jp>
2025-12-20 02:11:09 +0900

    feat: enable servicceusage.googleapis.com

--- terraform/api.tf
+++ terraform/api.tf
@@ -2,6 +2,10 @@ resource "google_project_service" "monitoring" {
   service = "monitoring.googleapis.com"
 }
 
+resource "google_project_service" "serviceusage" {
+  service = "serviceusage.googleapis.com"
+}
+
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
 }
