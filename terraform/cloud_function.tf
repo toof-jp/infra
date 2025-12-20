7f58fdbd6ddcd5c2716f02312adc2dd2c884e4f4
Fumiya Tokumasu <toof@toof.jp>
2025-12-21 01:11:17 +0900

    fix: set correct project

--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -1,6 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  project                     = var.project
-  name                        = "${var.project}-google-monitoring-notification-channel-discord-functions"
+  name                        = "${google_project.toof_infra.name}-google-monitoring-notification-channel-discord-functions"
   location                    = "US"
   uniform_bucket_level_access = true
 }
