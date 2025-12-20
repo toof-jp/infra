5f29e7d93764d8958a9f07205e676f94029bdde5
Fumiya Tokumasu <toof@toof.jp>
2025-12-21 01:06:27 +0900

    chore: terraform fmt

--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "bucket" {
-  project  = var.project
-  name     = "${var.project}-google-monitoring-notification-channel-discord-functions"
-  location = "US"
+  project                     = var.project
+  name                        = "${var.project}-google-monitoring-notification-channel-discord-functions"
+  location                    = "US"
   uniform_bucket_level_access = true
 }
 
