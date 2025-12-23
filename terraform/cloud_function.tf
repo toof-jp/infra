90b4a4a2ef80655bf39e6478ba50398de265eb60
Fumiya Tokumasu <toof@toof.jp>
2025-12-23 23:37:28 +0900

    feat: secret for discord webhook url

--- terraform/cloud_function.tf
+++ terraform/cloud_function.tf
@@ -3,3 +3,16 @@ resource "google_storage_bucket" "bucket" {
   location                    = "US"
   uniform_bucket_level_access = true
 }
+
+resource "google_secret_manager_secret" "discord_webhook_url" {
+  secret_id = "discord-webhhok-url"
+  replication {
+    auto {}
+  }
+  depends_on = [google_project_service.secretmanager]
+}
+
+resource "google_secret_manager_secret_version" "discord_webhook_url" {
+  secret      = google_secret_manager_secret.discord_webhook_url.id
+  secret_data = discord_webhook.webhook.url
+}
